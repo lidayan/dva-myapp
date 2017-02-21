a6fabfac9c1d65a5e03716c1e4b0082d51644d81
dayan.li <840286247@qq.com>
2017-02-21 20:58:47 +0800

    修改model的方法

--- src/models/count.js
+++ src/models/count.js
@@ -9,22 +9,24 @@ export default {
   },
   reducers: {
     add(state) {
-      const newCurrent = state.current + 1;
       return { ...state,
-        record: newCurrent > state.record ? newCurrent : state.record,
-        current: newCurrent,
+        record: state.record + 1,
+        current: state.current + 1,
       };
     },
     minus(state) {
-      return { ...state, current: state.current - 1};
+      return { ...state, 
+        record: state.record + 1,
+        current: state.current - 1,
+      };
     },
   },
-  effects: {
-    *add(action, { call, put }) {
-      yield call(delay, 1000);
-      yield put({ type: 'minus' });
-    },
-  },
+  // effects: {
+  //   *add(action, { call, put }) {
+  //     yield call(delay, 1000);
+  //     yield put({ type: 'minus' });
+  //   },
+  // },
   subscriptions: {
     keyboardWatcher({ dispatch }) {
       key('⌘+up, ctrl+up', () => { dispatch({type:'add'}) });
